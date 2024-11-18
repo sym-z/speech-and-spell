774ac9740754a627d6ebb8ce913a7c4b61a537ce
Jack Sims <123803257+sym-z@users.noreply.github.com>
2024-11-18 13:23:52 -0800

    Ending sounds done!

--- game-code/speech-and-spell/scripts/speech_scene.gd
+++ game-code/speech-and-spell/scripts/speech_scene.gd
@@ -1,12 +1,24 @@
 extends Node2D
 @export var mouth : AnimatedSprite2D
 @export var backgroundImage : AnimatedSprite2D
+@export var goodSound : AudioStreamMP3
+@export var badSound : AudioStreamMP3
+@export var speaker : AudioStreamPlayer2D
+
 func _ready():
 	mouth.perform()
 func _on_mouth_end_perf():
 	mouth.visible = false;
 	print("GAME WON: ", Globals.gameWon)
 	if(Globals.gameWon):
-		backgroundImage.animation = "win"
+		goodEnd()
 	else:
-		backgroundImage.animation = "loss"
+		badEnd()
+func badEnd():
+	backgroundImage.animation = "loss"
+	speaker.stream = badSound
+	speaker.play()
+func goodEnd():
+	backgroundImage.animation = "win"
+	speaker.stream = goodSound
+	speaker.play()
